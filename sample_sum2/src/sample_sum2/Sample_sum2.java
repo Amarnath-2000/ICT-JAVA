b98fbaec996e7026e203c67bed52d20f9e45a491
Amarnath V <amarvijay2000@gmail.com>
2022-11-30 09:09:09 +0530

    sample_sum 2

---
+++ sample_sum2/src/sample_sum2/Sample_sum2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sample_sum2;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class Sample_sum2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int age=20;
+        int weight=80;
+        if(age>=18){
+            if(weight>50){
+                System.out.println("You are eligible to donate blood");
+            }
+        }
+    }
+    
+}
