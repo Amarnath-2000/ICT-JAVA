66d1e4ea12d25030d4459a13788987cb9feb2b5d
Amarnath V <amarvijay2000@gmail.com>
2022-11-30 08:46:25 +0530

    JAVA

---
+++ Java program 1/src/java/program/pkg1/JavaProgram1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java.program.pkg1;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class JavaProgram1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int age=20;
+        if(age>18);
+        System.out.println("Age is greater than 18");
+    }
+    
+}}
