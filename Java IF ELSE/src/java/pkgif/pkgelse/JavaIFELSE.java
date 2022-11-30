2db240eb44bdb50ec3e78fc24f0aab27410dc7a7
Amarnath V <amarvijay2000@gmail.com>
2022-11-30 08:59:16 +0530

    if else

---
+++ Java IF ELSE/src/java/pkgif/pkgelse/JavaIFELSE.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java.pkgif.pkgelse;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class JavaIFELSE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number=13;
+        if(number%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+        }
+    }
+    
+}
