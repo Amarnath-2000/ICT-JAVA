1bc9a0f1d08bffcccd1d5883116c9287d87f2203
Amarnath V <amarvijay2000@gmail.com>
2022-11-30 21:48:15 +0530

    Update Final_class.java

--- final_class/src/final_class/Final_class.java
+++ final_class/src/final_class/Final_class.java
@@ -27,8 +27,35 @@ System.out.println("Actors will play");
         System.out.println("Actors will act");
     }
 }
+class Malayalam-actor extends actor
+{
+    Malayalam-actor()
+    {
+        super.name="Mohanlal";
+        super.place-of-birth="Kerala";
+    }
+    Malayalam actor(string name1,string place)
+    {
+        super.name=name1;
+        super.place-of-birth=place;
+    }
+    String actor-state;
+    void actor-state()
+    {
+        System.out.println("Actor state is kerala");
+    }
+    void actor-state(string state)
+    {
+        System.out.println("Actor state is"+state);
+    }
+    void acting()
+    {
+        System.out.println("Malayalam actor will act well")c;
+    }
+}
 
-    /**
+            
+            
      * @param args the command line arguments
      */
     
