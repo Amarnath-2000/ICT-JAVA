43f27b34fdfe09d083a4381d8a9ae9187d5eef3c
Amarnath V <amarvijay2000@gmail.com>
2022-11-30 23:06:02 +0530

    car-project

---
+++ car-project/src/car/car.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class car {
+    public static void main(String[] args){
+        car Audi=new car()
+         Audi.car-brand="Audi";
+         Audi.car-model="Audi a4";
+         Audi.engine-number="556677";
+         Audi.chase-number="445566";
+         Audi.get-details();
+    car inova=new car();
+    inova.car-brand="inova";
+    inova.car-model="inova";
+    inova.engine-number="223344";
+    inova.chase-number="112345";
+    inova.get-details();
+    }
+}    
+        
+    
+             
+
