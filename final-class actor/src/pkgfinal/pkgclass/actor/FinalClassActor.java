39b2849d6815d1aa593fcec984ea80940058bb60
Amarnath V <amarvijay2000@gmail.com>
2022-11-30 22:35:10 +0530

    actor.java

---
+++ final-class actor/src/pkgfinal/pkgclass/actor/FinalClassActor.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfinal.pkgclass.actor;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class FinalClassActor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name;
+        String date_of_birth;
+        String place_of_birth;
+        String occupation;
+        String spouse;
+        String children;
+        String activities;
+        String awards;
+        int active_years;
+        void playing()
+        {
+            System.out.println("Actors will play");
+        }
+        void acting()
+        {
+            System.out.println("Actors will act");
+        }
+    }
+    class malayalam-actor extends actor
+    {
+        Malayalam-actor()
+        {
+            super.name="Mohanlal";
+            super.place-of-birth="kerala";
+        }
+        Malayalam actor(string name1,string place)
+        {
+    super.name=name1;
+    super.place-of-birth=place;
+        }
+        String actor-state;
+        void actor-state()
+        {
+            System.out.println("Actor state is kerala");
+        }
+        void actor-state(string state)
+        {
+            System.out.println("Actor state is:"+state);
+            {
+                void acting();
+                {
+                    System.out.println("Malayalam actor will act well");
+                }
+            }
+            
+                }
+            }
+        }
+    
+    
+}
